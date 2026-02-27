a1a7b1438e2af42942650ba10d9701f9ad136379
SemenTeplov <eburg-severca@mail.ru>
2026-02-27 21:26:46 +0500

    2-collector-json: update project

--- telemetry/collector/src/main/java/app/mapping/MappingHubEvent.java
+++ telemetry/collector/src/main/java/app/mapping/MappingHubEvent.java
@@ -2,8 +2,8 @@ package app.mapping;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
+import org.mapstruct.ValueMapping;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceAddedEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceRemovedEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceTypeAvro;
@@ -12,6 +12,7 @@ import ru.yandex.practicum.kafka.telemetry.event.ScenarioRemovedEventAvro;
 
 import telemetry.messages.DeviceAddedEventProto;
 import telemetry.messages.DeviceRemovedEventProto;
+import telemetry.messages.DeviceTypeProto;
 import telemetry.messages.ScenarioAddedEventProto;
 import telemetry.messages.ScenarioRemovedEventProto;
 
@@ -20,13 +21,12 @@ public interface MappingHubEvent {
     @Mapping(target = "type", source = "type")
     DeviceAddedEventAvro toDeviceAddedEventAvro(DeviceAddedEventProto event);
 
+    @ValueMapping(target = "null", source = "UNRECOGNIZED")
+    DeviceTypeAvro map(DeviceTypeProto protoType);
+
     DeviceRemovedEventAvro toDeviceRemovedEventAvro(DeviceRemovedEventProto event);
 
     ScenarioAddedEventAvro toScenarioAddedEventAvro(ScenarioAddedEventProto event);
 
     ScenarioRemovedEventAvro toScenarioRemovedEventAvro(ScenarioRemovedEventProto event);
-
-    default DeviceTypeAvro map(DeviceAddedEventProto event) {
-        return DeviceTypeAvro.valueOf(event.getType().toString());
-    }
 }
