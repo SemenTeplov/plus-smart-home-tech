b992d17f36cbedece93c9b1bf0c44644f68ffeab
SemenTeplov <eburg-severca@mail.ru>
2026-03-08 23:28:12 +0500

    3-aggregator: update project

--- telemetry/aggregator/src/app/java/app/config/KafkaConfig.java
+++ telemetry/aggregator/src/app/java/app/config/KafkaConfig.java
@@ -38,7 +38,7 @@ public class KafkaConfig {
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class);
-        configs.put(ConsumerConfig.GROUP_ID_CONFIG, "aggregation-starter-group");
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, "hub");
 
         return new KafkaConsumer<>(configs);
     }
@@ -50,7 +50,7 @@ public class KafkaConfig {
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorsSnapshotDeserializer.class);
-        configs.put(ConsumerConfig.GROUP_ID_CONFIG, "aggregation-starter-group");
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, "hub");
 
         return new KafkaConsumer<>(configs);
     }
