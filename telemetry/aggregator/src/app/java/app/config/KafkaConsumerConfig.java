74893498dc20f6d0355e78195947a491ebe793c5
SemenTeplov <eburg-severca@mail.ru>
2026-03-13 23:36:05 +0500

    3-aggregator: update project

--- telemetry/aggregator/src/app/java/app/config/KafkaConsumerConfig.java
+++ telemetry/aggregator/src/app/java/app/config/KafkaConsumerConfig.java
@@ -1,23 +1,17 @@
 package app.java.app.config;
 
-import kafka.serializer.GeneralAvroSerializer;
 import kafka.serializer.SensorEventDeserializer;
 import kafka.serializer.SensorsSnapshotDeserializer;
 
-import lombok.RequiredArgsConstructor;
-
-import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
@@ -26,8 +20,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@RequiredArgsConstructor
-public class KafkaConfig {
+@EnableKafka
+public class KafkaConsumerConfig {
     @Value("${kafka.bootstrap-servers}")
     private String server;
 
@@ -40,35 +34,24 @@ public class KafkaConfig {
     @Value("${kafka.offset.rest-config}")
     private String offsetRest;
 
-    @Bean("eventConsumer")
-    public Consumer<String, SensorEventAvro> eventConsumer() {
+    @Bean
+    public ConsumerFactory<String, SensorEventAvro> eventConsumer() {
         Map<String, Object> configs = consumerConfig();
 
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class);
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, sensorGroup);
 
-        return new KafkaConsumer<>(configs);
+        return new DefaultKafkaConsumerFactory<>(configs);
     }
 
-    @Bean("snapshotConsumer")
-    public Consumer<String, SensorsSnapshotAvro> snapshotConsumer() {
+    @Bean
+    public ConsumerFactory<String, SensorsSnapshotAvro> snapshotConsumer() {
         Map<String, Object> configs = consumerConfig();
 
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorsSnapshotDeserializer.class);
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, snapshotGroup);
 
-        return new KafkaConsumer<>(configs);
-    }
-
-    @Bean
-    public Producer<String, SensorsSnapshotAvro> snapshotProducer() {
-        Map<String, Object> configs = new HashMap<>();
-
-        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
-        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
-
-        return new KafkaProducer<>(configs);
+        return new DefaultKafkaConsumerFactory<>(configs);
     }
 
     private Map<String, Object> consumerConfig() {
--- telemetry/aggregator/src/app/java/app/starter/AggregationStarter.java
+++ telemetry/aggregator/src/app/java/app/starter/AggregationStarter.java
@@ -5,22 +5,26 @@ import lombok.extern.slf4j.Slf4j;
 
 import app.java.app.service.AggregatorService;
 
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.errors.WakeupException;
+//import org.apache.kafka.clients.consumer.Consumer;
+//import org.apache.kafka.clients.consumer.ConsumerRecord;
+//import org.apache.kafka.clients.consumer.ConsumerRecords;
+//import org.apache.kafka.clients.producer.Producer;
+//import org.apache.kafka.clients.producer.ProducerRecord;
+//import org.apache.kafka.common.errors.WakeupException;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
-import java.time.Duration;
-import java.util.List;
+//import java.time.Duration;
+//import java.util.ArrayList;
+//import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Slf4j
 @Component
@@ -28,51 +32,67 @@ import java.util.Optional;
 public class AggregationStarter {
     private final AggregatorService service;
 
-    private final Consumer<String, SensorEventAvro> eventConsumer;
+    private final CopyOnWriteArrayList<SensorEventAvro> sensors = new CopyOnWriteArrayList<>();
 
-    private final Producer<String, SensorsSnapshotAvro> snapshotProducer;
+    private final KafkaTemplate<String, SensorsSnapshotAvro> template;
 
-    private volatile boolean running = true;
-
-    @Value("${kafka.values.timeout}")
-    private int consumeTimeout;
-
-    @Value("${kafka.topics.sensor}")
-    private String sensorTopic;
-
+//    private final Consumer<String, SensorEventAvro> eventConsumer;
+//
+//    private final Producer<String, SensorsSnapshotAvro> snapshotProducer;
+//
+//    private volatile boolean running = true;
+//
+//    @Value("${kafka.values.timeout}")
+//    private int consumeTimeout;
+//
+//    @Value("${kafka.topics.sensor}")
+//    private String sensorTopic;
+//
     @Value("${kafka.topics.snapshot}")
     private String snapshotTopic;
 
     public void start() {
-        eventConsumer.subscribe(List.of(sensorTopic));
+        for (SensorEventAvro sensor : sensors) {
+            Optional<SensorsSnapshotAvro> snapshot = service.updateState(sensor);
 
-        try {
-            while (running) {
-                ConsumerRecords<String, SensorEventAvro> records =
-                        eventConsumer.poll(Duration.ofMillis(consumeTimeout));
-
-                for (ConsumerRecord<String, SensorEventAvro> record : records) {
-                    Optional<SensorsSnapshotAvro> sensor = service.updateState(record.value());
-
-                    sensor.ifPresent(sensorsSnapshotAvro ->
-                        snapshotProducer.send(new ProducerRecord<>(snapshotTopic, null, sensorsSnapshotAvro)));
-                }
-            }
-
-        } catch (WakeupException e) {
-
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        } finally {
-            snapshotProducer.flush();
-            eventConsumer.commitSync();
-            eventConsumer.close();
-            snapshotProducer.close();
+            snapshot.ifPresent(sensorsSnapshotAvro -> template.send(snapshotTopic, sensorsSnapshotAvro));
         }
+
+
+//        eventConsumer.subscribe(List.of(sensorTopic));
+//
+//        try {
+//            while (running) {
+//                ConsumerRecords<String, SensorEventAvro> records =
+//                        eventConsumer.poll(Duration.ofMillis(consumeTimeout));
+//
+//                for (ConsumerRecord<String, SensorEventAvro> record : records) {
+//                    Optional<SensorsSnapshotAvro> sensor = service.updateState(record.value());
+//
+//                    sensor.ifPresent(sensorsSnapshotAvro ->
+//                        snapshotProducer.send(new ProducerRecord<>(snapshotTopic, null, sensorsSnapshotAvro)));
+//                }
+//            }
+//
+//        } catch (WakeupException e) {
+//
+//        } catch (Exception e) {
+//            log.error(e.getMessage());
+//        } finally {
+//            snapshotProducer.flush();
+//            eventConsumer.commitSync();
+//            eventConsumer.close();
+//            snapshotProducer.close();
+//        }
     }
 
-    public void stop() {
-        running = false;
-        eventConsumer.wakeup();
+//    public void stop() {
+//        running = false;
+//        eventConsumer.wakeup();
+//    }
+
+    @KafkaListener(topics = "${kafka.topics.sensor}", groupId = "${kafka.group.sensor-group}")
+    public void handler(SensorEventAvro event) {
+        sensors.addIfAbsent(event);
     }
 }
