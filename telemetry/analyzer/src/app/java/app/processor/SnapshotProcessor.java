430b63311e63bb6160bbfaec8ba5b2d1bf68d4be
SemenTeplov <eburg-severca@mail.ru>
2026-03-22 21:27:26 +0500

    4-analyzer: update project

--- telemetry/analyzer/src/app/java/app/processor/SnapshotProcessor.java
+++ telemetry/analyzer/src/app/java/app/processor/SnapshotProcessor.java
@@ -63,7 +63,9 @@ public class SnapshotProcessor {
 
         event.getSensorsState().values().forEach(o -> {
                 actions.stream()
+                        .peek(a -> log.info(a.getType()))
                         .filter(a -> o.getClass().equals(a.getActionClass()))
+                        .peek(a -> log.info(a.getType()))
                         .forEach(a -> a.sendAction(o,
                                 conditionsDto.stream()
                                         .filter(c -> c.getSensor().getId().equals(a.getType())).toList(),
