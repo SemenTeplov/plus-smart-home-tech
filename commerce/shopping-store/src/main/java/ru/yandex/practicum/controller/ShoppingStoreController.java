d3f1b7b2de08e9b85ad4ed9615459415177f0915
SemenTeplov <eburg-severca@mail.ru>
2026-04-13 19:05:05 +0500

    7-spring-cloud-microservices: add project

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/controller/ShoppingStoreController.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/controller/ShoppingStoreController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,8 @@ import ru.yandex.practicum.service.ShoppingStoreService;
 
 import java.util.List;
 
-@RestController("/api/v1/shopping-store")
+@RestController
+@RequestMapping("/api/v1/shopping-store")
 @RequiredArgsConstructor
 public class ShoppingStoreController {
     private final ShoppingStoreService shoppingStoreService;
--- commerce/warehouse/src/main/java/ru/yandex/practicum/controller/WarehouseController.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/controller/WarehouseController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ru.yandex.practicum.dto.AddProductToWarehouseRequest;
@@ -18,7 +19,8 @@ import ru.yandex.practicum.dto.NewProductInWarehouseRequest;
 import ru.yandex.practicum.dto.ShoppingCartDto;
 import ru.yandex.practicum.service.WarehouseService;
 
-@RestController("/api/v1/warehouse")
+@RestController
+@RequestMapping("/api/v1/warehouse")
 @RequiredArgsConstructor
 public class WarehouseController {
 
