e84332bde1ef1e4a9b48746a2fd8fa4618064759
SemenTeplov <eburg-severca@mail.ru>
2026-04-13 18:23:48 +0500

    7-spring-cloud-microservices: add project

--- commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/ProductMapper.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/ProductMapper.java
@@ -4,12 +4,13 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import ru.yandex.practicum.dto.DimensionDto;
 import ru.yandex.practicum.dto.NewProductInWarehouseRequest;
 import ru.yandex.practicum.persistence.entity.Product;
 import ru.yandex.practicum.persistence.entity.Dimension;
 
-@Mapper
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ProductMapper {
     @Mapping(target = "dimension", qualifiedByName = "toDimension")
     Product toProduct(NewProductInWarehouseRequest request);
