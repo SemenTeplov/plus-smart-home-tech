86d0f169cbb4dab1cad03495b59c0f1974b89864
SemenTeplov <eburg-severca@mail.ru>
2026-04-13 18:38:40 +0500

    7-spring-cloud-microservices: add project

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/Main.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/Main.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
