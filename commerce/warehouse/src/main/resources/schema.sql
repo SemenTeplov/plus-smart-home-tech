7b404ecbab3769566bd2b95c1b148da04110ec23
SemenTeplov <eburg-severca@mail.ru>
2026-04-14 14:25:15 +0500

    7-spring-cloud-microservices: add project

--- commerce/warehouse/src/main/resources/schema.sql
+++ commerce/warehouse/src/main/resources/schema.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS wares (
     product_id UUID PRIMARY KEY,
     fragile BOOLEAN NOT NULL,
     dimension_id UUID NOT NULL,
-    width DECIMAL(10, 8) NOT NULL,
-    height DECIMAL(10, 8) NOT NULL,
-    depth DECIMAL(10, 8) NOT NULL,
-    weight DECIMAL(10, 8) NOT NULL,
+    width DECIMAL(10, 2) NOT NULL,
+    height DECIMAL(10, 2) NOT NULL,
+    depth DECIMAL(10, 2) NOT NULL,
+    weight DECIMAL(10, 2) NOT NULL,
     quantity INTEGER NOT NULL
 );
