2e95ac4ac9ba1365477b20a3409fc7b44d7c5880
SemenTeplov <eburg-severca@mail.ru>
2026-04-14 13:14:02 +0500

    7-spring-cloud-microservices: add project

--- infra/config-server/src/main/resources/config/telemetry/warehouse/schema.sql
+++ infra/config-server/src/main/resources/config/telemetry/warehouse/schema.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS wares (
     product_id UUID PRIMARY KEY,
     fragile BOOLEAN NOT NULL,
     dimension_id UUID NOT NULL,
-    width DOUBLE NOT NULL,
-    height DOUBLE NOT NULL,
-    depth DOUBLE NOT NULL,
-    weight DOUBLE NOT NULL,
+    width DECIMAL(10, 8) NOT NULL,
+    height DECIMAL(10, 8) NOT NULL,
+    depth DECIMAL(10, 8) NOT NULL,
+    weight DECIMAL(10, 8) NOT NULL,
     quantity INTEGER NOT NULL
-)
+);
