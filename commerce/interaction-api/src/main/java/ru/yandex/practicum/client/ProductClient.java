713d19cc8914037bd6118ab592b4837bbe57f2f5
SemenTeplov <eburg-severca@mail.ru>
2026-04-15 21:29:44 +0500

    7-spring-cloud-microservices: add project

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/client/ProductClient.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/client/ProductClient.java
@@ -4,12 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import ru.yandex.practicum.dto.BookedProductsDto;
 import ru.yandex.practicum.dto.ShoppingCartDto;
 
 @FeignClient(name = "warehouse")
 public interface ProductClient {
 
     @PostMapping("/check")
-    BookedProductsDto checkProducts(@RequestBody ShoppingCartDto shoppingCartDto);
+    void checkProducts(@RequestBody ShoppingCartDto shoppingCartDto);
 }
